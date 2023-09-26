9fb5d0dbc373f4690ab0e99c90d49579a21ff616
Martin Kromsten <kromsten@pm.me>
2023-09-26 16:38:47 +0200

    auto-suggestion

---
+++ site/src/lib/web3/misc/localChainInfo.ts
@@ -0,0 +1,76 @@
+import { PUBLIC_CONSUMER_CHAIN_ENDPOINT, PUBLIC_SECRET_CHAIN_ENDPOINT } from "$env/static/public"
+
+export const getLocalSecretChainInfo = (
+    chainId: string,
+    endpoint?: string,
+    nameSuffix?: string
+) => {
+
+    if (!endpoint) {
+      if (chainId == "test-1") {
+        endpoint = PUBLIC_SECRET_CHAIN_ENDPOINT;
+        nameSuffix ??= "Test 1"
+      } else if (chainId == "test-2") {
+        endpoint = PUBLIC_CONSUMER_CHAIN_ENDPOINT;
+        nameSuffix ??= "Test 2"
+      }
+    }
+
+
+    return {
+        "rpc": endpoint!,
+        "rest": endpoint!,
+        "chainId": chainId,
+        "chainName": "Local Secret" + nameSuffix,
+        "chainSymbolImageUrl": "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/secret/chain.png",
+        "stakeCurrency": {
+          "coinDenom": "SCRT",
+          "coinMinimalDenom": "uscrt",
+          "coinDecimals": 6,
+          "coinGeckoId": "secret",
+          "coinImageUrl": "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/secret/uscrt.png"
+        },
+        "walletUrl": "https://wallet.keplr.app/chains/secret-network",
+        "walletUrlForStaking": "https://wallet.keplr.app/chains/secret-network",
+        "bip44": {
+          "coinType": 529
+        },
+        "alternativeBIP44s": [
+          {
+            "coinType": 118
+          }
+        ],
+        "bech32Config": {
+          "bech32PrefixAccAddr": "secret",
+          "bech32PrefixAccPub": "secretpub",
+          "bech32PrefixValAddr": "secretvaloper",
+          "bech32PrefixValPub": "secretvaloperpub",
+          "bech32PrefixConsAddr": "secretvalcons",
+          "bech32PrefixConsPub": "secretvalconspub"
+        },
+        "currencies": [
+          {
+            "coinDenom": "SCRT",
+            "coinMinimalDenom": "uscrt",
+            "coinDecimals": 6,
+            "coinGeckoId": "secret",
+            "coinImageUrl": "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/secret/uscrt.png"
+          }
+        ],
+        "feeCurrencies": [
+          {
+            "coinDenom": "SCRT",
+            "coinMinimalDenom": "uscrt",
+            "coinDecimals": 6,
+            "coinGeckoId": "secret",
+            "coinImageUrl": "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/secret/uscrt.png",
+            "gasPriceStep": {
+              "low": 0.1,
+              "average": 0.25,
+              "high": 0.5
+            }
+          }
+        ],
+        "features": ["secretwasm", "authz-msg-revoke-fixed"]
+      }
+}
--- site/src/lib/web3/wallets/keplr.ts
+++ site/src/lib/web3/wallets/keplr.ts
@@ -1,3 +1,4 @@
+import { getLocalSecretChainInfo } from "../misc/localChainInfo";
 import type { WalletAccount, WalletConnector } from "../types";
 import type { Window as KeplrWindow } from "@keplr-wallet/types";
 
@@ -5,14 +6,30 @@ declare global {
   interface Window extends KeplrWindow {}
 }
 
+
+export const suggestKeplr = async (chainId : string | string[]) => {
+  chainId = Array.isArray(chainId) ? chainId[0] : chainId;
+  await window.keplr!.experimentalSuggestChain(getLocalSecretChainInfo(chainId))
+} 
+
+
 export const connectKeplr : WalletConnector = async (chainId : string | string[]) => {
+    
     try {
-        await window.keplr!.enable(chainId);
-        return true;
-      } catch (e : any) {
-        console.error(e.message)
-        return false;
-      }
+      await suggestKeplr(chainId);
+    } catch (e : any) {
+      console.error(e.message)
+      return false;
+    }
+  
+  
+    try {
+      await window.keplr!.enable(chainId);
+      return true;
+    } catch (e : any) {
+      console.error(e.message)
+      return false;
+    }
   }
   
 export const getKeplrAccount = async (chainId: string) : Promise<WalletAccount> => {
